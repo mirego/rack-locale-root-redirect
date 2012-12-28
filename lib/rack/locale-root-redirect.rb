c6f15d30641cbe3d9a1af377b2da158c9ec20a35
Rémi Prévost <remi@exomel.com>
2012-12-28 16:38:26 -0500

    Fix redirect host

--- lib/rack/locale-root-redirect.rb
+++ lib/rack/locale-root-redirect.rb
@@ -2,7 +2,7 @@ require "rack/accept"
 
 module Rack
   class LocaleRootRedirect
-    VERSION = "0.0.2"
+    VERSION = "0.0.3"
 
     def initialize(app, locales = {})
       @locales = locales
@@ -20,7 +20,7 @@ module Rack
         redirect_lang = language_matcher.best_of(@available_locales) || @default_locale
 
         status = 302
-        headers["Location"] = "#{env["HTTP_HOST"]}#{@locales[redirect_lang.to_sym]}"
+        headers["Location"] = "#{env["SERVER_PROTOCOL"]}://#{env["HTTP_HOST"]}#{@locales[redirect_lang.to_sym]}"
       end
 
       [status, headers, response]
