7b7ac036707cbc9d3a1d9a15730cca8b2174eb27
Rémi Prévost <remi@exomel.com>
2013-07-24 09:11:14 -0400

    Add support for query string in root URL

--- lib/rack/locale-root-redirect.rb
+++ lib/rack/locale-root-redirect.rb
@@ -14,13 +14,13 @@ module Rack
     def call(env)
       status, headers, response = @app.call(env)
 
-      if env["REQUEST_URI"] == "/"
+      if match_data = %r[\A/(?<query_string>\?.*|\Z)].match(env["REQUEST_URI"])
         language_matcher = env['rack-accept.request'].language
         language_matcher.first_level_match = true
         redirect_lang = language_matcher.best_of(@available_locales) || @default_locale
 
         status = 302
-        headers["Location"] = @locales[redirect_lang.to_sym]
+        headers["Location"] = @locales[redirect_lang.to_sym] + match_data[:query_string]
       end
 
       [status, headers, response]
